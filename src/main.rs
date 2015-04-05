476fbb0af4bd19cc81814afbd8276352b50f6162
Michael Pankov <work@michaelpankov.com>
2015-04-05 21:33:14 +0300

    mmap the file and print it as &[u8]

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
+#![feature(libc)]
+
+extern crate libc;
+
+use std::ffi::CString;
+
 use std::env;
 use std::io::Write;
 use std::process;
 
+use libc::types::common::c95::c_void;
+
 fn read_print_file(path: &str) {
-    ;
+    let cstr = CString::new(path).unwrap();
+    let fd = unsafe {
+        libc::funcs::posix88::fcntl::open(
+            cstr.as_ptr(),
+            libc::consts::os::posix88::O_RDONLY,
+            0)
+    };
+
+    let address = unsafe {
+        libc::funcs::posix88::mman::mmap(
+            0 as *mut c_void,
+            4096,
+            libc::consts::os::posix88::PROT_READ,
+            libc::consts::os::posix88::MAP_PRIVATE
+          | libc::consts::os::extra::MAP_POPULATE,
+            fd,
+            0
+            )
+    };
+
+    let buffer : &[u8] = unsafe {
+        std::slice::from_raw_parts(address as *const u8, 4096)
+    };
+
+    println!("{:?}", buffer);
+
+    unsafe {
+        libc::funcs::posix88::unistd::close(fd);
+    }
 }
 
 fn main() {
@@ -13,5 +49,5 @@ fn main() {
         writeln!(&mut stderr, "Usage: rexdump <file>").unwrap();
         process::exit(1);
     }
-    read_print_file(&args[0]);
+    read_print_file(&args[1]);
 }
