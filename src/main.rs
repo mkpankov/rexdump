2a80e6b57b489bff2c3c6b3ce4aa5dfa543fbfcf
Michael Pankov <work@michaelpankov.com>
2015-04-11 22:33:48 +0300

    Implement printing skeleton

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,62 @@ use libc::consts::os::posix88 as posix88_c;
 use libc::consts::os::extra;
 use libc::funcs::c95::stdio;
 
-fn read_print_file(path: &str, stderr: &mut Stderr) -> Result<(), ()> {
+fn print_offset(offset: i64) {
+
+}
+
+fn print_hex(buffer: &[u8], buffer_size: i64, line_width: i64) {
+
+}
+
+fn align_delimiter(line_size: i64, line_width_elements: i64) {
+
+}
+
+fn print_chars(buffer: &[u8], line_size: i64) {
+
+}
+
+fn print_contents(buffer: &[u8], buffer_size: i64, offset: i64) {
+    if buffer_size == 0 {
+        return;
+    };
+
+    let line_width_elements = 16;
+    let mut remaining_buffer_size = buffer_size;
+    let mut line_number = 0;
+    let mut current_offset = offset;
+
+    for line in buffer.chunks(line_width_elements as usize) {
+        let line_size = if remaining_buffer_size > line_width_elements {
+            line_width_elements
+        } else {
+            remaining_buffer_size
+        };
+        if line_size == 0 {
+            break;
+        }
+
+        print_offset(line_number * line_width_elements + current_offset);
+
+        print_hex(
+            line,
+            line_size,
+            line_width_elements);
+
+        align_delimiter(line_size, line_width_elements);
+
+        print_chars (line, line_size);
+
+        println!("|");
+
+        line_number += 1;
+        current_offset += line_size;
+        remaining_buffer_size -= line_size;
+    }
+}
+
+fn read_print_file(path: &str) -> Result<(), ()> {
     let c_path = CString::new(path).unwrap();
     let fd = unsafe {
         posix88_f::fcntl::open(
@@ -74,7 +129,7 @@ fn read_print_file(path: &str, stderr: &mut Stderr) -> Result<(), ()> {
             std::slice::from_raw_parts(address as *const u8, 4096)
         };
 
-        println!("{:?}", buffer);
+        print_contents(buffer, std::num::cast(map_size).unwrap(), offset);
 
         let result = unsafe {
             posix88_f::mman::munmap(
@@ -108,5 +163,5 @@ fn main() {
         process::exit(1);
     }
     let s : String = args.nth(1).unwrap();
-    read_print_file(&s, & mut stderr).unwrap();
+    read_print_file(&s).unwrap();
 }
