2d173395802e043b2688e250de5c3cafe5c1ce31
Michael Pankov <work@michaelpankov.com>
2015-04-11 20:02:35 +0300

    Use better argument handling
    No need to collect everything first

--- src/main.rs
+++ src/main.rs
@@ -46,11 +46,12 @@ fn read_print_file(path: &str) {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut args = env::args();
     let mut stderr = std::io::stderr();
     if args.len() != 2 {
         writeln!(&mut stderr, "Usage: rexdump <file>").unwrap();
         process::exit(1);
     }
-    read_print_file(&args[1]);
+    let s : String = args.nth(1).unwrap();
+    read_print_file(&s, & mut stderr).unwrap();
 }
